662148b897987d6c35adc2a86ad471f1171d38b8
BRUNO <brunotisistemas@gmail.com>
2017-08-20 19:03:00 -0300

    adicionando arquivos 2

--- src/br/com/git/App.java
+++ src/br/com/git/App.java
@@ -12,7 +12,16 @@ package br.com.git;
 public class App {
     
     public static void main(String[] args) {
-        System.out.println("Utilizando o git atraves do netbeans");
+        System.out.println("Utilizando o git atraves do netbeansss"
+                + "- Criar um novo projeto Java;\n" +
+"- Inicializar um repositório git local para o projeto Java;\n" +
+"- Controlar mudanças no diretório de trabalho e index de forma visual;\n" +
+"- Realizar o commit das modificações;\n" +
+"- Cancelar (reverter) mudanças do diretório de trabalho e/ou Stating area;\n" +
+"- Visualizar histórico de mudanças de um arquivo;\n" +
+"- Clonar (baixar) um repositório do github para o repositório local, e importa-lo como um projeto no NetBeans;\n" +
+"- Recuperar (pull) mudanças do repositório no github para o respositório local;\n" +
+"- Notificar (push) mudanças do repositório local para o remoto no github;");
     }
     
 }
